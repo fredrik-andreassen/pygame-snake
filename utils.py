a700c8219277d3383c2edea5e72e49abdb4ee7c1
Fredrik Aas Andreassen <98fredrik@live.no>
2022-12-14 22:43:50 +0100

    Fjernet utskrift

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import sys, re
 
 def get_args() -> dict[str, str]:
     arg_line = ' '.join(sys.argv[1:])
-    print(arg_line)
 
     arg_patterns = {'debug':   r'(-d|--debug)\s+([1-9][0-9]{0,2})',
                     'players': r'(-p|--players)\s+([1-4])'}
